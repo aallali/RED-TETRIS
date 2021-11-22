c0b59274beb00285738b73bcf0b963bb7061b505
Abdollah Allali (Golden) <aallali@student.1337.ma>
2021-11-22 18:55:13 +0100

    [UPDATE] : rework code :  app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Home from './pages/home'
 import Lobby from './pages/lobby'
-import Error from './component/Error';
+import Error from './component/error';
 import {
 	getPlayerNickname
 } from './reducers/player.reducer';
 import PlayBoard from "./pages/playboard";
 import { useAppDispatch, useAppSelector } from "./app/hooks"
 import { getError } from './reducers/error.reducer';
-import { SET_ERROR } from './actions';
+import { SET_ERROR } from './app/actions';
 import { socket } from "./app/hooks"
 import { useCallback, useEffect } from 'react';
 import { getGameMode, getGameTitle } from './reducers/game.reducer';
+import {useSelector} from "react-redux";
+import {store} from "./app/store";
 
 
 const HashParser = (window_hash: string) => {
@@ -29,12 +31,12 @@ const namevalidtor = (u: string, r: string) => {
 }
 function App() {
 	const dispatch = useAppDispatch()
-	const playerNickname = useAppSelector(getPlayerNickname)
 	const playerIngame = useAppSelector(getGameTitle)
+	const playerNickname = useAppSelector(getPlayerNickname)
 	const gameMode = useAppSelector(getGameMode)
 	const error = useAppSelector(getError)
-
-	const checkhash = useCallback(() => {
+	const checkhash = () => {
+		const playername = store.getState().player.nickname
 		console.log("CHECK HASH CALLED ", window.location.hash)
 		socket.emit("PLAYER_LEFT")
 		if (window.location.hash.substring(1)) {
@@ -42,8 +44,8 @@ function App() {
 			if (checkHash && checkHash.groups) {
 				const { roomname, username } = checkHash.groups
 				if (namevalidtor(username, roomname)) {
-					console.log(playerNickname, playerIngame, username)
-					if (playerNickname && username !== playerNickname) {
+					console.log("===>",  playername, playerIngame, username)
+					if (playername && username !== playername) {
 						dispatch(SET_ERROR({ title: "Error :", message: "there is an username already set to this browser, try to leave first to register new account" }))
 					} else {
 						dispatch(SET_ERROR({ title: "", message: "" }))
@@ -65,7 +67,7 @@ function App() {
 			socket.emit("PLAYER_LEFT")
 
 		}
-	}, [playerIngame, playerNickname])
+	}
 	useEffect(() => {
 		checkhash()
 		window.removeEventListener("hashchange", () => { })
