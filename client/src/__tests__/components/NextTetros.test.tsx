9a4b1b994e1aaf6c21501e35af2cb38cba2f277c
Abdollah Allali (Golden) <aallali@student.1337.ma>
2021-11-29 17:07:45 +0100

    [Test] : unit test : NextTetros Component : [DONE]

--- client/src/__tests__/components/NextTetros.test.tsx
+++ client/src/__tests__/components/NextTetros.test.tsx
@@ -1,11 +1,18 @@
 import renderer from "react-test-renderer";
 import NextTetros from "../../components/NextTetros"
 import { Provider } from 'react-redux';
-import { store } from '../../app/store';
 
-test("App render test ", () => {
-	const tree = renderer.create(<Provider store={store}>
-		<NextTetros />
-	</Provider>).toJSON();
-	expect(tree).toMatchSnapshot();
-});
+import { aallaliStore } from '../../test-utils'
+
+
+describe("_ NextTetro Component test", () => {
+	test("Next Tetro snaphot ", () => {
+		const store = aallaliStore({ game: { tetros: ['L', 'Z', 'T', 'O', 'T'] } })
+
+		const tree = renderer.create(<Provider store={store}>
+			< NextTetros />
+		</Provider>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+})
