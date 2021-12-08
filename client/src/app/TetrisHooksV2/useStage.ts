37f6eca4ac9bff15e10f471218221f7cb30c6bc4
Abdollah Allali (Golden) <aallali@student.1337.ma>
2021-12-08 11:32:59 +0100

    Update useStage.ts

--- client/src/app/TetrisHooksV2/useStage.ts
+++ client/src/app/TetrisHooksV2/useStage.ts
@@ -45,16 +45,23 @@ export const useStage = (player: PLAYER, resetPlayer: (a: keyof typeof TETROMINO
 			);
 
 			// Then draw the tetromino
-			player.tetromino.forEach((row, y) => {
-				row.forEach((value, x) => {
-					if (value !== 0) {
-						newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? 'merged' : 'clear'}`];
-					}
+			try {
+
+				player.tetromino.forEach((row, y) => {
+					row.forEach((value, x) => {
+						if (value !== 0) {
+
+							newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? 'merged' : 'clear'}`];
+						}
+					});
 				});
-			});
+			} catch (error) {
+				console.log(player.pos)
+				throw error
+			}
+
 
 			if (player.collided) {
-
 				resetPlayer(tetro);
 				return sweepRows(newStage);
 			}
